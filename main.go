75c2ff7710009a9665eec6b90d02d005c40be896
Andrey <deff46rus@yandex.ru>
2025-02-04 10:29:25 +0300

    last version

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -11,18 +13,20 @@ import (
 const serverURL = "http://localhost:8088"
 
 func main() {
+	token := ""
+	readTocken(&token)
 	bot, err := tgbotapi.NewBotAPI(token) // Создаём бота с токеном
 	if err != nil {
 		log.Panic(err)
 	}
 	bot.Debug = false
-	log.Printf("Бот %s включен", bot.Self.UserName)  
+	log.Printf("Бот %s включен", bot.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {  // Пропуск всех типов обновлений, кроме сообщений
+		if update.Message == nil { // Пропуск всех типов обновлений, кроме сообщений
 			continue
 		}
 		// Обработка команд
@@ -50,16 +54,34 @@ func main() {
 	}
 }
 
+func readTocken(token *string) {
+	file, err := os.Open("token.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		*token = scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+
+	}
+}
+
 // Функция работы с громкостью
 func volume(update tgbotapi.Update, bot *tgbotapi.BotAPI) error {
-	args := update.Message.CommandArguments() // Получаем аргументы команды
+	args := update.Message.CommandArguments()                  // Получаем аргументы команды
 	resp, err := http.Get(serverURL + "/volume?level=" + args) /// Отправка GET-запроса на сервер с параметрами
 	if err != nil {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ошибка сервера")
 		bot.Send(msg)
 		return err
 	}
-	defer resp.Body.Close() // Освобождение ресурсов тела ответа
+	defer resp.Body.Close()                // Освобождение ресурсов тела ответа
 	response, err := io.ReadAll(resp.Body) // Чтение всего ответа
 	if err != nil {
 		return err
